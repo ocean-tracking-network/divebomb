570732252203e97798e52ad74fb3c0fe64993777
Alex Nunes <alex.et.nunes@gmail.com>
2018-02-27 12:25:45 -0500

    Docs conf change for pathing on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../divebomb'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath('.')), 'divebomb'))
 
 
 # -- General configuration ------------------------------------------------
