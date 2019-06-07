ab5717591a353fe3538f50b7944d3e2dacf17b3b
Alex Nunes <alex.et.nunes@gmail.com>
2019-06-07 11:25:11 -0300

    setup fix

--- setup.py
+++ setup.py
@@ -15,11 +15,12 @@ setup(
     version='1.1.0',
     description='divebomb dive classification algorithm',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Alex Nunes',
     include_package_data=True,
     author_email='alex.et.nunes@gmail.com',
     url='https://github.com/ocean-tracking-network/divebomb',
     download_url='https://github.com/ocean-tracking-network/divebomb',
-    license=license,
+    license='GPLv2',
     packages=find_packages(exclude=('tests', 'docs'))
 )
